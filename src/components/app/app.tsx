6e564435a4cb420ccb0462bbc288aa6d115d4665
Evgeny Zhdanov <evgeny1halvin@yandex.ru>
2021-10-17 16:18:55 +0200

    created starting file structure

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import appStyles from './app.module.css';
+
+function App() {
+  return (
+    <div className={appStyles.app}>
+    </div>
+  );
+}
+
+export default App;
